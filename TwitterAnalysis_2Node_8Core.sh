b4d2debf478c0a92fffc87d1757ddb77d8ef98e9
Navaneeth <=>
2019-04-07 02:18:54 +1000

    Update script to reflect correct core count
    Signed-off-by: Navaneeth <=>

--- TwitterAnalysis_2Node_8Core.sh
+++ TwitterAnalysis_2Node_8Core.sh
@@ -3,4 +3,4 @@
 #SBATCH --nodes=2
 #SBATCH --ntasks-per-node=4
 module load Python/3.4.3-goolf-2015a
-mpirun -np 1 python TwitterAnalysis_Parallel.py
+mpirun -np 8 python TwitterAnalysis_Parallel.py
