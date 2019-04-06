56f15a0fff0ed7d1d0c05ee6f2279d48bb277113
Navaneeth <=>
2019-04-07 02:00:42 +1000

    Scatter data to all cores. Collect processed data list from cores.
    Signed-off-by: Navaneeth <=>

---
+++ TwitterAnalysis_Parallel.py
@@ -0,0 +1,79 @@
+import sys
+sys.path.append('./Model')
+from MelbGrid import MelbGrid
+from Summary import Summary
+import json
+import time
+from mpi4py import MPI
+
+comm = MPI.COMM_WORLD
+rank = comm.rank
+size = comm.size
+
+
+startTime = time.time()
+
+def processTwitterFile(mySummary, rank, cores):
+    # Load the BigTwitter.json file and populate MelbGrid
+    #with open('TwitterFiles/BigTwitter.json', encoding="utf8") as twitterFileHandle:
+
+    # Load the SmallTwitter.json file and populate MelbGrid
+    #with open('TwitterFiles/SmallTwitter.json', encoding="utf8") as twitterFileHandle:
+
+    # Load the TinyTwitter.json file and populate MelbGrid
+    with open('TwitterFiles/TinyTwitter.json', encoding="utf8") as twitterFileHandle:
+
+    # Load the Sample TestTwitter.json file and populate MelbGrid
+    #with open('TwitterFiles/TestTwitter.json', encoding="utf8") as twitterFileHandle:
+        for lineNum, line in enumerate(twitterFileHandle):
+            if lineNum > 0:
+                if lineNum % cores == rank:
+                    if "coordinates" in str(line):
+                        if str(line[-2]) == ',':
+                            line = line[:-2]
+                        elif str(line[-3:-1]) == "]}":
+                            break
+                        mySummary.totalTweetsProcessed += 1
+                        tweetDetails = json.loads(line)
+                        mySummary.melbGrid.processTweet(tweetDetails)
+
+
+# Create an empty list of size = number of cores
+summaryList = [None] * comm.size
+
+# Load the MelbGrid json and parse the data to melbGrid
+with open('Config/MelbGrid.json', encoding="utf8") as melbGridConfigFile:
+    melbGridConfig = json.load(melbGridConfigFile)
+
+# Inilialize the Melb Grid from the MelbGrid.json file data
+melbGrid = MelbGrid(melbGridConfig["features"])
+finalMelbGrid = MelbGrid(melbGridConfig["features"])
+
+
+if rank == 0:
+    summaryList = []
+    for i in range(size):
+        summaryList.append(Summary(melbGrid))
+    print("Scattering list: ")
+    print(summaryList)
+else:
+    summaryList = None
+
+mySummary = comm.scatter(summaryList, root = 0)
+
+print("After Scatter")
+print(mySummary)
+
+processTwitterFile(mySummary, rank, size)
+
+endTime = time.time()
+
+mySummary.executionTime = endTime - startTime
+
+processedList = comm.gather(mySummary, root = 0)
+
+if rank == 0:
+    print("Received Data")
+    print(processedList)
+
+print("\n\nExecution Time: " + str(endTime - startTime) + " s")
