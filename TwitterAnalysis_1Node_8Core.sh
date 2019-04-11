a3ac6989f936ea8fb4d579bc0c29899965c60f6d
Navaneeth <=>
2019-04-11 21:59:53 +1000

    use physical partition for 1 Node 8 core case
    Signed-off-by: Navaneeth <=>

--- TwitterAnalysis_1Node_8Core.sh
+++ TwitterAnalysis_1Node_8Core.sh
@@ -2,5 +2,6 @@
 #SBATCH --time=00:20:00
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=8
+#SBATCH --partition=physical
 module load Python/3.4.3-goolf-2015a
 time mpirun -np 8 python TwitterAnalysis_Parallel.py
